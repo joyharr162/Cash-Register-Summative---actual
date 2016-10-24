aeb36f113368d1857776665bb9cb271b22d5b290
joyharr162 <joyharr162@ed.amdsb.ca>
2016-10-24 12:50:04 -0400

    Add project files.

---
+++ Cash Register Summative/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Cash_Register_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.carrotMuffinLabel = new System.Windows.Forms.Label();
+            this.chocChipMuffinLabel = new System.Windows.Forms.Label();
+            this.blueberryMuffinLabel = new System.Windows.Forms.Label();
+            this.carrotMuffinBox = new System.Windows.Forms.TextBox();
+            this.chocChipMuffinBox = new System.Windows.Forms.TextBox();
+            this.blueberryMuffinBox = new System.Windows.Forms.TextBox();
+            this.subtotalButton = new System.Windows.Forms.Button();
+            this.taxButton = new System.Windows.Forms.Button();
+            this.grandTotalButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.grandTotalLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.tenderLabel = new System.Windows.Forms.Label();
+            this.tenderBox = new System.Windows.Forms.TextBox();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.tryCatchLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // carrotMuffinLabel
+            // 
+            this.carrotMuffinLabel.AutoSize = true;
+            this.carrotMuffinLabel.Location = new System.Drawing.Point(15, 72);
+            this.carrotMuffinLabel.Name = "carrotMuffinLabel";
+            this.carrotMuffinLabel.Size = new System.Drawing.Size(102, 13);
+            this.carrotMuffinLabel.TabIndex = 0;
+            this.carrotMuffinLabel.Text = "Carrot Muffins $3.25";
+            // 
+            // chocChipMuffinLabel
+            // 
+            this.chocChipMuffinLabel.AutoSize = true;
+            this.chocChipMuffinLabel.Location = new System.Drawing.Point(15, 103);
+            this.chocChipMuffinLabel.Name = "chocChipMuffinLabel";
+            this.chocChipMuffinLabel.Size = new System.Drawing.Size(146, 13);
+            this.chocChipMuffinLabel.TabIndex = 1;
+            this.chocChipMuffinLabel.Text = "Chocolate Chip Muffins $3.50";
+            // 
+            // blueberryMuffinLabel
+            // 
+            this.blueberryMuffinLabel.AutoSize = true;
+            this.blueberryMuffinLabel.Location = new System.Drawing.Point(15, 133);
+            this.blueberryMuffinLabel.Name = "blueberryMuffinLabel";
+            this.blueberryMuffinLabel.Size = new System.Drawing.Size(118, 13);
+            this.blueberryMuffinLabel.TabIndex = 2;
+            this.blueberryMuffinLabel.Text = "Blueberry Muffins $2.90";
+            // 
+            // carrotMuffinBox
+            // 
+            this.carrotMuffinBox.Location = new System.Drawing.Point(173, 65);
+            this.carrotMuffinBox.Name = "carrotMuffinBox";
+            this.carrotMuffinBox.Size = new System.Drawing.Size(27, 20);
+            this.carrotMuffinBox.TabIndex = 3;
+            // 
+            // chocChipMuffinBox
+            // 
+            this.chocChipMuffinBox.Location = new System.Drawing.Point(173, 100);
+            this.chocChipMuffinBox.Name = "chocChipMuffinBox";
+            this.chocChipMuffinBox.Size = new System.Drawing.Size(27, 20);
+            this.chocChipMuffinBox.TabIndex = 4;
+            // 
+            // blueberryMuffinBox
+            // 
+            this.blueberryMuffinBox.Location = new System.Drawing.Point(173, 133);
+            this.blueberryMuffinBox.Name = "blueberryMuffinBox";
+            this.blueberryMuffinBox.Size = new System.Drawing.Size(27, 20);
+            this.blueberryMuffinBox.TabIndex = 5;
+            // 
+            // subtotalButton
+            // 
+            this.subtotalButton.BackColor = System.Drawing.Color.Salmon;
+            this.subtotalButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.subtotalButton.Location = new System.Drawing.Point(18, 158);
+            this.subtotalButton.Name = "subtotalButton";
+            this.subtotalButton.Size = new System.Drawing.Size(75, 23);
+            this.subtotalButton.TabIndex = 6;
+            this.subtotalButton.Text = "Subtotal";
+            this.subtotalButton.UseVisualStyleBackColor = false;
+            this.subtotalButton.Click += new System.EventHandler(this.subtotalButton_Click);
+            // 
+            // taxButton
+            // 
+            this.taxButton.BackColor = System.Drawing.Color.Salmon;
+            this.taxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.taxButton.Location = new System.Drawing.Point(18, 196);
+            this.taxButton.Name = "taxButton";
+            this.taxButton.Size = new System.Drawing.Size(75, 23);
+            this.taxButton.TabIndex = 7;
+            this.taxButton.Text = "Tax";
+            this.taxButton.UseVisualStyleBackColor = false;
+            this.taxButton.Click += new System.EventHandler(this.taxButton_Click);
+            // 
+            // grandTotalButton
+            // 
+            this.grandTotalButton.BackColor = System.Drawing.Color.Salmon;
+            this.grandTotalButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grandTotalButton.Location = new System.Drawing.Point(18, 234);
+            this.grandTotalButton.Name = "grandTotalButton";
+            this.grandTotalButton.Size = new System.Drawing.Size(75, 23);
+            this.grandTotalButton.TabIndex = 8;
+            this.grandTotalButton.Text = "Grand Total";
+            this.grandTotalButton.UseVisualStyleBackColor = false;
+            this.grandTotalButton.Click += new System.EventHandler(this.grandTotalButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Location = new System.Drawing.Point(123, 158);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(0, 13);
+            this.subtotalLabel.TabIndex = 9;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(123, 187);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(0, 13);
+            this.taxLabel.TabIndex = 10;
+            // 
+            // grandTotalLabel
+            // 
+            this.grandTotalLabel.AutoSize = true;
+            this.grandTotalLabel.Location = new System.Drawing.Point(123, 221);
+            this.grandTotalLabel.Name = "grandTotalLabel";
+            this.grandTotalLabel.Size = new System.Drawing.Size(0, 13);
+            this.grandTotalLabel.TabIndex = 11;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.BackColor = System.Drawing.Color.Salmon;
+            this.receiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.receiptButton.Location = new System.Drawing.Point(242, 32);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(118, 23);
+            this.receiptButton.TabIndex = 12;
+            this.receiptButton.Text = "Print My Receipt";
+            this.receiptButton.UseVisualStyleBackColor = false;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // tenderLabel
+            // 
+            this.tenderLabel.AutoSize = true;
+            this.tenderLabel.Location = new System.Drawing.Point(21, 271);
+            this.tenderLabel.Name = "tenderLabel";
+            this.tenderLabel.Size = new System.Drawing.Size(123, 13);
+            this.tenderLabel.TabIndex = 14;
+            this.tenderLabel.Text = "Enter Amount Tendered:";
+            // 
+            // tenderBox
+            // 
+            this.tenderBox.Location = new System.Drawing.Point(173, 271);
+            this.tenderBox.Name = "tenderBox";
+            this.tenderBox.Size = new System.Drawing.Size(28, 20);
+            this.tenderBox.TabIndex = 15;
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.BackColor = System.Drawing.Color.Salmon;
+            this.newOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newOrderButton.Location = new System.Drawing.Point(242, 3);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(118, 23);
+            this.newOrderButton.TabIndex = 16;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = false;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Giddyup Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(13, 3);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(182, 39);
+            this.titleLabel.TabIndex = 17;
+            this.titleLabel.Text = "Best Muffins Ever!";
+            // 
+            // tryCatchLabel
+            // 
+            this.tryCatchLabel.AutoSize = true;
+            this.tryCatchLabel.Location = new System.Drawing.Point(15, 42);
+            this.tryCatchLabel.Name = "tryCatchLabel";
+            this.tryCatchLabel.Size = new System.Drawing.Size(0, 13);
+            this.tryCatchLabel.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(404, 303);
+            this.Controls.Add(this.tryCatchLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.tenderBox);
+            this.Controls.Add(this.tenderLabel);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.grandTotalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.grandTotalButton);
+            this.Controls.Add(this.taxButton);
+            this.Controls.Add(this.subtotalButton);
+            this.Controls.Add(this.blueberryMuffinBox);
+            this.Controls.Add(this.chocChipMuffinBox);
+            this.Controls.Add(this.carrotMuffinBox);
+            this.Controls.Add(this.blueberryMuffinLabel);
+            this.Controls.Add(this.chocChipMuffinLabel);
+            this.Controls.Add(this.carrotMuffinLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Best Muffins Ever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label carrotMuffinLabel;
+        private System.Windows.Forms.Label chocChipMuffinLabel;
+        private System.Windows.Forms.Label blueberryMuffinLabel;
+        private System.Windows.Forms.TextBox carrotMuffinBox;
+        private System.Windows.Forms.TextBox chocChipMuffinBox;
+        private System.Windows.Forms.TextBox blueberryMuffinBox;
+        private System.Windows.Forms.Button subtotalButton;
+        private System.Windows.Forms.Button taxButton;
+        private System.Windows.Forms.Button grandTotalButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label grandTotalLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label tenderLabel;
+        private System.Windows.Forms.TextBox tenderBox;
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label tryCatchLabel;
+    }
+}
+
---
+++ Cash Register Summative/Form1.cs
@@ -0,0 +1,135 @@
+﻿///Created by Joy Harris
+///October 14  2016
+///for the purpose of simulating a store cash register
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cash_Register_Summative
+{
+    public partial class Form1 : Form
+    {
+        //declare variables and constants
+        int ccmQuantity;
+        int cmQuantity;
+        int bmQuantity;
+        double change;
+        double subtotal;
+        double taxPrice;
+        double carrotPrice;
+        double chocolatePrice;
+        double blueberryPrice;
+        double tender;
+        const double CARROTCOST = 3.25;
+        const double CHOCCHIPCOST = 3.5;
+        const double BLUEBERRYCOST = 2.9;
+        const double TAX = .13;
+        const int ORDERNUMBER = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //assign number of each muffin to the appropriate variable
+            try
+            {
+              ccmQuantity = Convert.ToInt16(chocChipMuffinBox.Text);
+              cmQuantity = Convert.ToInt16(carrotMuffinBox.Text);
+              bmQuantity = Convert.ToInt16(blueberryMuffinBox.Text);
+            }
+            catch
+            {
+                //if letters are used, tell user to input integers only
+                tryCatchLabel.Text = "Please input integrs only";
+            }
+        }
+
+        private void subtotalButton_Click(object sender, EventArgs e)
+        {
+            //calculate the total before tax
+            carrotPrice = cmQuantity * CARROTCOST;
+            chocolatePrice = ccmQuantity * CHOCCHIPCOST;
+            blueberryPrice = bmQuantity * BLUEBERRYCOST;
+            subtotal = carrotPrice + blueberryPrice + chocolatePrice;
+            subtotalLabel.Text = Convert.ToString(subtotal);
+        }
+
+        private void taxButton_Click(object sender, EventArgs e)
+        {
+            //calculate tax
+            taxLabel.Text = Convert.ToString(TAX * subtotal);
+        }
+
+        private void grandTotalButton_Click(object sender, EventArgs e)
+        {
+            //calculate the total after tax
+            taxPrice = Convert.ToInt16(taxLabel.Text);
+            grandTotalLabel.Text = Convert.ToString(subtotal + taxPrice);
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            //make pens, fonts, etc.
+            Font drawFont = new Font("Consolas", 8, FontStyle.Bold);
+            Pen receiptPen = new Pen(Color.LightPink, 5);
+            SolidBrush fontBrush = new SolidBrush(Color.Black);
+            SolidBrush receiptBrush = new SolidBrush(Color.LightPink);
+            Graphics formGraphics = this.CreateGraphics();
+
+            //draw receipt
+            formGraphics.FillRectangle(receiptBrush, 200, 55, 260, 200);
+            Thread.Sleep(500);
+            formGraphics.DrawString("Best Muffins Ever\nOrder Nuumber " + ORDERNUMBER + " October 2016", drawFont, fontBrush, 200, 60);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Chocolate Chip Muffins  " + ccmQuantity + " @ $" + CHOCCHIPCOST, drawFont, fontBrush, 200, 90);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Carrot Muffins          " + cmQuantity + " @ $" + CARROTCOST, drawFont, fontBrush, 200, 110);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Blueberry Muffins       " + bmQuantity + " @ $" + BLUEBERRYCOST, drawFont, fontBrush, 200, 130);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Subtotal                " + ccmQuantity*CHOCCHIPCOST +cmQuantity*CARROTCOST +bmQuantity*BLUEBERRYCOST, drawFont, fontBrush, 200, 150);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Tax @ 13%               " + TAX*ccmQuantity *CHOCCHIPCOST + TAX*cmQuantity * CARROTCOST + TAX*bmQuantity * BLUEBERRYCOST, drawFont, fontBrush, 200, 170);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Grand Total             " + taxLabel.Text + subtotalLabel.Text ,drawFont, fontBrush, 200, 190);
+            Thread.Sleep(1000);
+            formGraphics.DrawString("Tendered                " + tenderBox.Text, drawFont, fontBrush, 200, 210);
+            Thread.Sleep(1000);
+
+            try
+            {
+                tender = Convert.ToInt16(tenderBox.Text);
+                change = tender - Convert.ToInt16(grandTotalLabel.Text);
+            }
+            catch
+            {
+                tryCatchLabel.Text = "Please input numbers only";
+            }
+
+            formGraphics.DrawString("Change                     " + change, drawFont, fontBrush, 200, 230);
+
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        {
+            //clear input and output areas
+            Graphics formGraphics = this.CreateGraphics();
+            carrotMuffinBox.Text = "";
+            chocChipMuffinBox.Text = "";
+            blueberryMuffinBox.Text = "";
+            subtotalLabel.Text = "";
+            grandTotalLabel.Text = "";
+            taxLabel.Text = "";
+            tenderBox.Text = "";
+            formGraphics.Clear(Color.PeachPuff);
+
+        }
+    }
+}
